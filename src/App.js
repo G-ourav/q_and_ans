be7e661750ccf26b0f82a6f50ee80a66d6392a12
G-ourav <85567438+G-ourav@users.noreply.github.com>
2024-08-09 01:55:48 +0530

    creacted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [start, setStart] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="h-screen bg-slate-100 ">
+        <div className="  flex text-center justify-center ">
+          <h1 className=" text-3xl bg-slate-300 text-blue rounded-2xl p-5 uppercase">
+            Q and Ans
+          </h1>
+        </div>
+
+        {start ? (
+          <div className="p-10  flex justify-center my-auto ">
+            <button
+              className=" bg-slate-500 rounded-xl p-5  uppercase  text-5xl"
+              onClick={() => {
+                setStart(false);
+              }}
+            >
+              start test
+            </button>
+          </div>
+        ) : (
+          <div className="p-10  flex justify-center my-auto ">
+            <div
+              className=" p-10  bg-slate-300  rounded-2xl
+             "
+            >
+              {/* quetion */}
+              <p>adsfad</p>
+              {/* options */}
+              <p>asgfdf</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
