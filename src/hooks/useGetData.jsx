774f141edf7cc7b4931d82b779d50bcedde3222b
gourav <85567438+G-ourav@users.noreply.github.com>
2024-08-09 18:31:21 +0530

    Create useGetData.jsx

---
+++ src/hooks/useGetData.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+// import { setalert_ } from "../redux/login_state";
+// import { useDispatch, useSelector } from "react-redux";
+
+function useGetData(url) {
+  // const dispatch = useDispatch();
+  const [get_user_data, setget_user_data] = useState(null);
+  const [Get_error, setGet_error] = useState(null);
+
+  useEffect(() => {
+    const get_user_data = async () => {
+      try {
+        const res = await fetch(
+          "https://opentdb.com/api.php?amount=10"
+          // , {
+          //   method: "GET",
+          //   headers: {
+          //     "Content-Type": "application/json",
+          //   },
+          // }
+        );
+
+        if (!res.ok) {
+          console.log("API is not working");
+          throw new Error("Network response was not ok");
+        }
+
+        const get_res = await res.json();
+        // const Data = JSON.stringify(get_res);
+        setget_user_data(get_res);
+        // console.log(Data, get_res, res);
+        console.log(res, get_res);
+        // dispatch(setalert_({ message_: get_res.message, status_: get_res.status }));
+      } catch (error) {
+        setGet_error(error);
+        console.error("An error occurred:", error);
+      }
+    };
+    get_user_data();
+  }, [url]);
+
+  return [get_user_data, Get_error];
+}
+
+export default useGetData;
