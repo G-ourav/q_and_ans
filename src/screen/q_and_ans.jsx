f9ea51744fb27e6f470fc303522eb01f12223b02
G-ourav <85567438+G-ourav@users.noreply.github.com>
2024-08-11 02:58:04 +0530

    Update q_and_ans.jsx

--- src/screen/q_and_ans.jsx
+++ src/screen/q_and_ans.jsx
@@ -2,15 +2,12 @@ import React, { useState } from "react";
 import Option from "./option";
 
 const Q_and_ans = ({
-  question,
-  category,
+  question_content,
   setQuestion_no,
   question_no,
   all_ans,
-  difficulty,
   score,
   setScore,
-  correct_answer,
   ans_flag,
   setAns_flag,
 }) => {
@@ -18,17 +15,16 @@ const Q_and_ans = ({
   const [answerd_n, setAnswerd_n] = useState(0);
 
   const ans_check = () => {
-    setAns_flag(!ans_flag);
     console.log(answerd_n - 1, all_ans[answerd_n - 1]);
     if (answerd_n !== 0) {
       console.log(answerd_n - 1, all_ans[answerd_n - 1]);
       if (question_no <= 9) {
-        if (all_ans[answerd_n - 1] === correct_answer) {
+        if (all_ans[answerd_n - 1] === question_content?.correct_answer) {
           setScore([
             ...score,
             {
               user_answer: all_ans[answerd_n - 1],
-              correct_answer: correct_answer,
+              correct_answer: question_content?.correct_answer,
               user_score: score[score?.length - 1]?.user_score + 1 || 0,
             },
           ]);
@@ -37,7 +33,7 @@ const Q_and_ans = ({
             ...score,
             {
               user_answer: all_ans[answerd_n - 1],
-              correct_answer: correct_answer,
+              correct_answer: question_content?.correct_answer,
               user_score: score[score?.length - 1]?.user_score || 0,
             },
           ]);
@@ -47,39 +43,44 @@ const Q_and_ans = ({
       } else {
         setQuestion_no(9);
       }
+      setAns_flag(!ans_flag);
     }
   };
 
   return (
-    <div className=" h-full  w-screen p-10  ">
+    <div className="   p-10  ">
       <div
         className=" p-10  bg-slate-300  rounded-2xl
    "
       >
-        <div className=" md:flex text-lg justify-between gap-2">
+        <div className=" md:flex  pb-5 text-lg justify-between gap-2">
           <div className="p-2">
             <div className=" rounded-2xl p-2 shadow-2xl   shadow-green-400 bg-teal-200 ">
-              category: {category}
+              category: {question_content?.category}
             </div>
           </div>
           <div className="flex py-2 gap-4 ">
-            {difficulty === "hard" ? (
+            {question_content?.difficulty === "hard" ? (
               <div className=" rounded-full p-5 shadow-2xl shadow-orange-500   bg-red-200 ">
                 hard
               </div>
-            ) : difficulty === "medium" ? (
+            ) : question_content?.difficulty === "medium" ? (
               <div className=" rounded-full p-5 shadow-2xl shadow-orange-500 bg-yellow-200 ">
                 medium
               </div>
             ) : (
-              <div className=" rounded-full p-5 shadow-2xl shadow-green-950 bg-teal-200 ">easy</div>
+              <div className=" rounded-full p-5 shadow-2xl shadow-green-950 bg-teal-200 ">
+                easy
+              </div>
             )}
           </div>
         </div>
         {/* quetion */}
-        <p>{question}</p>
+        <p className="p-5 rounded-2xl text-white bg-gray-800">
+          {question_content?.question}
+        </p>
         {/* options */}
-        <div className="grid gap-2 py-4  ">
+        <div className="grid  gap-4 py-6  ">
           {all_ans?.map((e, i) => {
             return (
               <Option
@@ -95,7 +96,7 @@ const Q_and_ans = ({
           })}
         </div>
         <button
-          className=" bg-slate-500 rounded-xl p-2  uppercase  text-3xl"
+          className=" bg-slate-800 text-white shadow-2xl shadow-black   px-3 py-1 rounded-xl  uppercase  text-lg"
           onClick={() => {
             ans_check();
           }}
