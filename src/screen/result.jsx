2fe6a4d2242073c0d7823407096d4650809029a8
gourav <85567438+G-ourav@users.noreply.github.com>
2024-08-10 17:07:27 +0530

    Update result.jsx

--- src/screen/result.jsx
+++ src/screen/result.jsx
@@ -1,44 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Result = ({
-  score,
-  setQuestion_no,
-  question_no,
-  ans_flag,
-  setAns_flag,
-}) => {
+const Result = ({ score, setQuestion_no, question_no, ans_flag, setAns_flag }) => {
+  useEffect(() => {
+    if (score) {
+      console.log(score);
+    }
+  }, []);
   return (
-    <div>
-      {" "}
-      <div className="col-span-1    ">
-        <div className="rounded-2xl text-left bg-slate-300  p-5">
-          <p className="text-xl">Correct! 🎉</p>
-          <p className="">
-            Great job! You got it right. Keep up the good work and get ready for
-            the next question.
-          </p>
-        </div>
-        <div className=" grid grid-cols-2 ">
-          <div className="">
-            <button
-              className=" bg-slate-500 rounded-xl p-2  uppercase  text-xl"
-              onClick={() => {
-                console.log(question_no);
-                // setQuestion_no(question_no < 9 ? question_no + 1 : 9);
-              }}
-            >
-              end
-            </button>
+    <div className=" h-full  w-screen p-10  ">
+      <div
+        className=" p-10  bg-slate-300  rounded-2xl
+ "
+      >
+        <div className="col-span-1    ">
+          <div className="rounded-2xl text-left bg-slate-300  p-5">
+            <div className="flex  justify-between ">
+              {score[score?.length - 1]?.correct_answer ===
+              score[score?.length - 1]?.user_answer ? (
+                <p className="text-3xl">Correct! 🎉🤩</p>
+              ) : (
+                <p className="text-3xl">Incorrect! 😧</p>
+              )}
+              <button
+                className="  bg-slate-500 rounded-xl p-2  uppercase  text-xl"
+                onClick={() => {
+                  console.log(question_no);
+                  // setQuestion_no(question_no < 9 ? question_no + 1 : 9);
+                }}
+              >
+                end test
+              </button>
+            </div>
+            {score[score?.length - 1]?.correct_answer === score[score?.length - 1]?.user_answer ? (
+              <p className=" text-lg">
+                Great job! You got it right. Keep up the good work and get ready for the next
+                question.
+              </p>
+            ) : (
+              <p className=" text-lg">
+                Great effort! That was close, but not quite right. Don’t worry, you’re doing
+                great—let’s move on to the next question and keep up the good work!
+              </p>
+            )}
+
+            <h1 className="  text-lg text-blue  pt-2 ">
+              Total Questions Served : {score?.length}{" "}
+            </h1>
+            <h1 className="  text-lg  text-blue  py-1 ">
+              Total Correct Questions : {score[score?.length - 1]?.user_score}
+            </h1>
           </div>
           <div className=" ">
             <button
               className=" bg-slate-500 rounded-xl   p-2  uppercase  text-xl"
               onClick={() => {
-                console.log(question_no);
                 setAns_flag(!ans_flag);
-                // if (question_no <= 9) {
-                //   setQuestion_no((prev) => prev + 1);
-                // } else setQuestion_no((prev) => prev);
               }}
             >
               next quetion
