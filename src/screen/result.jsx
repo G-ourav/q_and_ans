a0ed1523376454e01b56657a1023ac4aeafc2487
G-ourav <85567438+G-ourav@users.noreply.github.com>
2024-08-11 02:58:07 +0530

    Update result.jsx

--- src/screen/result.jsx
+++ src/screen/result.jsx
@@ -1,28 +1,49 @@
 import React, { useEffect } from "react";
 
-const Result = ({ score, setQuestion_no, question_no, ans_flag, setAns_flag }) => {
+const Result = ({
+  score,
+  setQuestion_no,
+  question_no,
+  ans_flag,
+  setAns_flag,
+}) => {
   useEffect(() => {
     if (score) {
       console.log(score);
     }
   }, []);
+
   return (
-    <div className=" h-full  w-screen p-10  ">
+    <div className="  p-10  ">
       <div
         className=" p-10  bg-slate-300  rounded-2xl
  "
       >
         <div className="col-span-1    ">
           <div className="rounded-2xl text-left bg-slate-300  p-5">
-            <div className="flex  justify-between ">
+            <div className="md:flex  pb-5 justify-between ">
               {score[score?.length - 1]?.correct_answer ===
               score[score?.length - 1]?.user_answer ? (
-                <p className="text-3xl">Correct! 🎉🤩</p>
+                <p
+                  className=" text-3xl pb-5  flex "
+                  data-wow-duration="2s"
+                  data-wow-delay="1s"
+                >
+                  Correct! 🎉
+                  <p className="  wow animate__animated animate__heartBeat  ">
+                    🤩
+                  </p>
+                </p>
               ) : (
-                <p className="text-3xl">Incorrect! 😧</p>
+                <p className="text-3xl  pb-5  flex ">
+                  Incorrect!
+                  <p className="  wow animate__animated animate__heartBeat  ">
+                    😧
+                  </p>
+                </p>
               )}
               <button
-                className="  bg-slate-500 rounded-xl p-2  uppercase  text-xl"
+                className=" bg-slate-800 text-white   px-3 py-1  rounded-xl  uppercase  text-lg"
                 onClick={() => {
                   console.log(question_no);
                   // setQuestion_no(question_no < 9 ? question_no + 1 : 9);
@@ -31,19 +52,23 @@ const Result = ({ score, setQuestion_no, question_no, ans_flag, setAns_flag }) =
                 end test
               </button>
             </div>
-            {score[score?.length - 1]?.correct_answer === score[score?.length - 1]?.user_answer ? (
-              <p className=" text-lg">
-                Great job! You got it right. Keep up the good work and get ready for the next
-                question.
-              </p>
-            ) : (
-              <p className=" text-lg">
-                Great effort! That was close, but not quite right. Don’t worry, you’re doing
-                great—let’s move on to the next question and keep up the good work!
-              </p>
-            )}
+            <div>
+              {score[score?.length - 1]?.correct_answer ===
+              score[score?.length - 1]?.user_answer ? (
+                <p className=" text-lg">
+                  Great job! You got it right. Keep up the good work and get
+                  ready for the next question.
+                </p>
+              ) : (
+                <p className=" text-lg">
+                  Great effort! That was close, but not quite right. Don’t
+                  worry, you’re doing great—let’s move on to the next question
+                  and keep up the good work!
+                </p>
+              )}
+            </div>
 
-            <h1 className="  text-lg text-blue  pt-2 ">
+            <h1 className="  text-lg text-blue  pt-5 ">
               Total Questions Served : {score?.length}{" "}
             </h1>
             <h1 className="  text-lg  text-blue  py-1 ">
@@ -52,12 +77,12 @@ const Result = ({ score, setQuestion_no, question_no, ans_flag, setAns_flag }) =
           </div>
           <div className=" ">
             <button
-              className=" bg-slate-500 rounded-xl   p-2  uppercase  text-xl"
+              className=" bg-slate-800 text-white rounded-xl   px-3 py-1  uppercase  text-lg"
               onClick={() => {
                 setAns_flag(!ans_flag);
               }}
             >
-              next quetion
+              next
             </button>
           </div>
         </div>
