8abc5fe96c7469f2f09dff096e8965f0046a8ca7
gourav <85567438+G-ourav@users.noreply.github.com>
2024-08-12 15:19:59 +0530

    Update app_stack.js

--- src/navigation/app_stack.js
+++ src/navigation/app_stack.js
@@ -4,7 +4,7 @@ import Total_result from "../screen/Total_result";
 import Q_and_ans from "../screen/q_and_ans";
 import Start from "../screen/Start";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const app_stack = () => {
   return (
@@ -13,6 +13,9 @@ const app_stack = () => {
       <Route path="/q and ans" element={<Q_and_ans />} />
       <Route path="/result" element={<Result />} />
       <Route path="/total result" element={<Total_result />} />
+      {/* Redirect any other path to the Start page */}
+      {/* Redirect any other path to the Start page */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
