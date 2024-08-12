1690ce88be1741e186faeda6f3b41584ebecdae7
G-ourav <85567438+G-ourav@users.noreply.github.com>
2024-08-12 14:39:16 +0530

    Update Start.jsx

--- src/screen/Start.jsx
+++ src/screen/Start.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { setStart, setTotal_no_question } from "../redux/Q_and_ansReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const Start = ({ setStart }) => {
+const Start = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { total_no_question } = useSelector((state) => state.Q_and_ans);
+
   return (
-    <div className=" h-full  w-screen p-10  ">
+    <div className="  p-10  ">
       <div
-        className=" p-10 flex flex-col gap-5  bg-slate-300  rounded-2xl
-   "
+        className=" p-10  bg-slate-300  rounded-2xl
+"
       >
-        <p className=" bg-slate-800 text-white shadow-2xl shadow-black   px-3 py-1 rounded-xl  uppercase  text-lg">
+        <p className=" bg-slate-800  text-white shadow-2xl shadow-black   px-3 py-1 rounded-xl  uppercase  text-lg">
           before starting the Quiz
         </p>
-        <div className=" flex flex-col gap-3  text-left">
+        <div className=" flex flex-col pt-4 gap-3  text-left">
           <p>
             <span className=" font-semibold">1 Start a Quiz:</span> Click on any
             quiz to begin playing. Select an Answer:
@@ -40,16 +47,34 @@ const Start = ({ setStart }) => {
             answers and learn from the explanations.{" "}
           </p>
         </div>
-        <div>
-          <button
-            className=" bg-slate-800 text-white shadow-2xl shadow-black   px-3 py-1 rounded-xl  uppercase  text-lg"
-            onClick={() => {
-              setStart(false);
-            }}
-          >
-            Start
-          </button>
-        </div>
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault(); // Prevent the default form submission behavior
+            navigate("/q and ans"); // Ensure the route is correctly formatted
+          }}
+        >
+          <div className="flex flex-col text-left gap-2 py-4">
+            <p>No of Questions you want</p>
+            <input
+              className="rounded-xl px-3 py-2"
+              required
+              type="number"
+              max={100}
+              min={1}
+              value={total_no_question}
+              onChange={(e) => {
+                dispatch(setTotal_no_question(e.target.value));
+              }}
+            />
+            <button
+              type="submit"
+              className="bg-slate-800 text-white shadow-2xl shadow-black px-3 py-1 rounded-xl uppercase text-lg"
+            >
+              Start
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
