a6ea706ec767b47d81237232a2a70ff9b4f5d0ad
G-ourav <85567438+G-ourav@users.noreply.github.com>
2024-08-12 14:39:21 +0530

    Update Total_result.jsx

--- src/screen/Total_result.jsx
+++ src/screen/Total_result.jsx
@@ -1,20 +1,70 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+ChartJS.register(ArcElement, Tooltip, Legend);
 
-const Total_result = ({ score }) => {
+const Total_result = () => {
+  const { score } = useSelector((state) => state.Q_and_ans);
+  console.log(score);
+  const data = {
+    labels: ["Total Correct", "Total Incorrect"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [
+          score?.length - score[score?.length - 1]?.user_score,
+          score[score?.length - 1]?.user_score,
+        ],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(75, 192, 192, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(75, 192, 192, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
-    <div>
-      {" "}
-      <div className="col-span-1    ">
-        <div className="rounded-2xl text-left bg-slate-300  p-5">
-          <h1 className=" text-xl bg-slate-500 text-blue  p-5 uppercase">
-            Total Questions Served : {score?.length}{" "}
-          </h1>
-          <h1 className=" text-xl bg-slate-500  text-blue  p-5 uppercase">
-            Total Correct Questions : {}
-          </h1>
-          <h1 className=" text-xl bg-slate-500  text-blue  p-5 uppercase">
-            Total Incorrect Questions : {}
-          </h1>
+    <div className="  p-10  ">
+      <div
+        className=" p-10  bg-slate-300  rounded-2xl
+ "
+      >
+        <div className="md:flex  pb-5 justify-between ">
+          <p
+            className=" text-3xl pb-5  flex "
+            data-wow-duration="2s"
+            data-wow-delay="1s"
+          >
+            Thank You for Taking Our Quiz! 🎉
+            <p className="  wow animate__animated animate__heartBeat  ">👏</p>
+          </p>
+        </div>
+        <div>
+          <p className=" text-lg">
+            We appreciate you taking the time to complete our quiz. Your
+            responses help us tailor our content and services to better meet
+            your needs.
+          </p>
+        </div>
+        <div className="grid md:grid-cols-2">
+          <div className="md:col-span-1 md:flex justify-center">
+            <Pie data={data} />
+          </div>
+          <div className="col-span-1    ">
+            <div className="rounded-2xl text-left bg-slate-300  p-5">
+              <h1 className="  text-lg text-blue  pt-5 ">
+                Total Questions Served : {score?.length}{" "}
+              </h1>
+              <h1 className="  text-lg  text-blue  py-1 ">
+                Total Correct Questions :{" "}
+                {score[score?.length - 1]?.user_score || 0}
+              </h1>
+              <h1 className="  text-lg  text-blue  py-1 ">
+                Total Incorrect Questions :{" "}
+                {score?.length - score[score?.length - 1]?.user_score || 0}
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
     </div>
